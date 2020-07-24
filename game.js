8bcbe0a2f59f672e22513d31bed5f4e3b9c59720
Son Dao <daohsn@gmail.com>
2020-07-23 22:29:36 -0700

    Refactoring the app.js file

--- game.js
+++ game.js
@@ -1,80 +1,79 @@
-
-var colors = ["red", "blue", "green", "yellow"];
+const colors = ["red", "green", "blue", "yellow"];
 var gamePattern = [];
 var userPattern = [];
 var gameLevel = 0;
-var numberOfClick = 0;
-
-// start the game at any keypress
-$(document).keypress(function() {
-  if (gameLevel === 0) {
-    newSequence();
-  }
-})
-
-// choose a random color and add it to the game pattern
-function newSequence() {
-  gameLevel++;
-  $("#level-title").text("Level " + gameLevel);
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomColor = colors[randomNumber];
-  gamePattern.push(randomColor);
-  playSound(randomColor);
-  playAnimation(randomColor);
-  // console.log(gamePattern)
-}
-
-// listen for the click and then compare it to the game pattern
+var userLevel = 0;
+//keypressed, start game
+  $(document).keypress(function() {
+    if (gameLevel === 0){
+      console.log("game started");
+      newSequence();
+    }
+});
+//click
 $(".btn").click(function() {
   var clickedColor = $(this).attr("id");
   userPattern.push(clickedColor);
   playSound(clickedColor);
   playAnimation(clickedColor);
 
-  numberOfClick++;
-  compareAt(numberOfClick);
-  // console.log(userPattern);
-});
-
+  userLevel++;
+  if (gameLevel !== 0) {
+    compareAtIndex(userLevel);
+  }
+})
+//add new sequence
+function newSequence() {
+  gameLevel++;
+  $("#level-title").text("Level " + gameLevel);
+  // push a random color to gamePattern[]
+  var index = Math.floor(Math.random() * 4);
+  var randomColor = colors[index];
+  gamePattern.push(randomColor);
+  // console.log(gamePattern);
+  playSound(randomColor);
+  playAnimation(randomColor);
+}
+//compareAt(index)
+function compareAtIndex(index) {
+  if (userPattern[index - 1] === gamePattern[index - 1]) {
+    if (index === gameLevel) {
+      //reset the player inputs
+      userLevel = 0;
+      userPattern = [];
+      //user got the patttern corect, create a new Level
+      setTimeout(function () {
+       newSequence();
+     }, 1000);
+    }
+  }
+  else {
+    gameOver();
+  }
+}
+//playSound
 function playSound(color) {
-  var audio = new Audio("sounds/" + color +'.mp3');
+  var audio = new Audio("sounds/" + color + ".mp3");
   audio.play();
 }
-
+//playAnimation
 function playAnimation(color) {
   $("#" + color).addClass("pressed");
-  setTimeout(function () {
-    $("#" + color).removeClass("pressed");
-  }, 100);
+  setTimeout(function() {
+    $("#" + color).removeClass("pressed"); }, 100);
 }
-
-// the function resets the board and modifies some css elements
-function gameOver () {
+//game over, reset the board
+function gameOver() {
   playSound("wrong");
   $("body").addClass("game-over");
   $("#level-title").text("Game Over, Press Any Key to Restart");
 
   setTimeout(function () {
     $("body").removeClass("game-over");
-  }, 200);
-  // reset the board
-  gameLevel = 0;
-  numberOfClick = 0;
+  }, 200)
+  // reset board
   gamePattern = [];
   userPattern = [];
-}
-// this function compares the user input to the game pattern at given index
-function compareAt(index) {
-  if(userPattern[index - 1] === gamePattern[index - 1]) {
-    if (index === gameLevel) {
-      // reset the user inputs and start newSequence
-      userPattern = [];
-      numberOfClick = 0;
-      setTimeout(function () {
-        newSequence();
-      }, 1000);
-    }
-  }
-  else
-   gameOver();
+  gameLevel = 0;
+  userLevel = 0;
 }
