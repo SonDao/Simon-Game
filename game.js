c6cd03b9077dba90867d01ef176dd21769e0bae5
Son Dao <daohsn@gmail.com>
2020-07-31 23:11:50 -0700

    changed vars

--- game.js
+++ game.js
@@ -1,79 +1,82 @@
 const colors = ["red", "green", "blue", "yellow"];
-var gamePattern = [];
-var userPattern = [];
-var gameLevel = 0;
-var userLevel = 0;
-//keypressed, start game
-  $(document).keypress(function() {
-    if (gameLevel === 0){
-      console.log("game started");
-      newSequence();
-    }
-});
-//click
+let compChoice = [];
+let userChoice = [];
+let gameLevel = 0;
+let userLevel = 0;
+
 $(".btn").click(function() {
-  var clickedColor = $(this).attr("id");
-  userPattern.push(clickedColor);
+
+  let clickedColor = $(this).attr("id");
+  userChoice.push(clickedColor);
+
   playSound(clickedColor);
   playAnimation(clickedColor);
 
   userLevel++;
-  if (gameLevel !== 0) {
-    compareAtIndex(userLevel);
+  compareAt(userLevel);
+
+});
+
+$(document).keypress(function() {
+  // stuff
+  if (gameLevel === 0) {
+    newCompChoice();
   }
-})
-//add new sequence
-function newSequence() {
+});
+
+function gameOver() {
+  // reset board
+  compChoice = [];
+  userChoice = [];
+  gameLevel = 0;
+  userLevel = 0;
+
+  playSound("wrong");
+
+  $("#level-title").text("Game Over, Press Any Key to Restart");
+  $("body").addClass("game-over");
+  setTimeout(function() {
+    $("body").removeClass("game-over");
+  }, 500);
+
+
+}
+
+function newCompChoice() {
   gameLevel++;
-  $("#level-title").text("Level " + gameLevel);
-  // push a random color to gamePattern[]
-  var index = Math.floor(Math.random() * 4);
-  var randomColor = colors[index];
-  gamePattern.push(randomColor);
-  // console.log(gamePattern);
+  const newText = "Level " + gameLevel;
+  $("#level-title").text(newText);
+  const randomIndex = Math.floor(Math.random() * 4);
+  const randomColor = colors[randomIndex];
+  compChoice.push(randomColor);
   playSound(randomColor);
   playAnimation(randomColor);
 }
-//compareAt(index)
-function compareAtIndex(index) {
-  if (userPattern[index - 1] === gamePattern[index - 1]) {
-    if (index === gameLevel) {
-      //reset the player inputs
+
+function compareAt(index) {
+  if (userChoice[index - 1] === compChoice[index - 1]) {
+    if (userLevel === gameLevel) {
       userLevel = 0;
-      userPattern = [];
-      //user got the patttern corect, create a new Level
-      setTimeout(function () {
-       newSequence();
-     }, 1000);
+      userChoice = [];
+      setTimeout(function() {
+        newCompChoice();
+      }, 500);
     }
-  }
-  else {
+
+  } else {
     gameOver();
   }
+
 }
-//playSound
+
 function playSound(color) {
-  var audio = new Audio("sounds/" + color + ".mp3");
+  let audio = new Audio("sounds/" + color + ".mp3");
   audio.play();
 }
-//playAnimation
+
 function playAnimation(color) {
   $("#" + color).addClass("pressed");
   setTimeout(function() {
-    $("#" + color).removeClass("pressed"); }, 100);
-}
-//game over, reset the board
-function gameOver() {
-  playSound("wrong");
-  $("body").addClass("game-over");
-  $("#level-title").text("Game Over, Press Any Key to Restart");
-
-  setTimeout(function () {
-    $("body").removeClass("game-over");
-  }, 200)
-  // reset board
-  gamePattern = [];
-  userPattern = [];
-  gameLevel = 0;
-  userLevel = 0;
+    $("#" + color).removeClass("pressed");
+  }, 100);
 }
